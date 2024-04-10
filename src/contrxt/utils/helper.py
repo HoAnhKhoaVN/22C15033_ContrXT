df5ac97e56c69c0970e17d31a2903ff32b4557bd
HoAnhKhoa <traincolab2131999v1@gmail.com>
2024-04-10 12:57:46 +0700

    add helper

--- src/contrxt/utils/helper.py
+++ src/contrxt/utils/helper.py
@@ -1,11 +1,41 @@
 import os
+from typing import List
 
 
-def union():
-    pass
+def union(
+    lst_1: List,
+    lst_2: List
+)-> int:
+    """Return total number of features from two lists.
 
-def jaccard_distance():
-    pass
+    Args:
+        lst_1 (List): _description_
+        lst_2 (List): _description_
+
+    Returns:
+        int: _description_
+    """
+    return len(set(lst_1).union(set(lst_2)))
+
+def jaccard_distance(
+    lst_1: List,
+    lst_2: List,
+)->int:
+    """Returns the Jaccard distance between two lists.
+
+    Args:
+        lst_1 (List): List to compare
+        lst_2 (List): List to compare
+
+    Returns:
+        int: Jaccard distance between the two lists.
+    """
+    set_1 , set_2 = set(lst_1), set(lst_2)
+    n_union = len(set_1.union(set_2))
+    n_intersection = len(set_1.intersection(set_2))
+    jaccard_idx = n_intersection / n_union
+
+    return 1 - jaccard_idx
 
 if __name__ == "__main__":
     pass
