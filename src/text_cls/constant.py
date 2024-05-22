470873fda5b56b8c7d5cdf5f294b661b496a460b
khoaha <traincolab2131999v1@gmail.com>
2024-05-22 19:34:54 +0700

    add constant path

--- src/text_cls/constant.py
+++ src/text_cls/constant.py
@@ -3,3 +3,6 @@ TWENTY_NEW_GROUP_TEST_PATH = "src/text_cls/dataset/20newsgroups/test.csv"
 
 VNTC_TRAIN_PATH = "src/text_cls/dataset/VNTC/train"
 VNTC_TEST_PATH = "src/text_cls/dataset/VNTC/test"
+
+VNTC_TRAIN_CSV_PATH = "src/text_cls/dataset/VNTC/train.csv"
+VNTC_TEST_CSV_PATH = "src/text_cls/dataset/VNTC/test.csv"
