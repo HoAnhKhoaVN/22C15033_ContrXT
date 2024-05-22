475cad3e5f91db49a94b8ed9b7f270bfeb1deba9
khoaha <traincolab2131999v1@gmail.com>
2024-05-22 19:34:28 +0700

    add demo run code

---
+++ scripts/text_cls/run_text_cls.sh
@@ -0,0 +1,5 @@
+
+python src/text_cls/text_cls.py --dataset "20newgroups" \
+                                --verbose true \
+                                --word true \
+                                --model bert
