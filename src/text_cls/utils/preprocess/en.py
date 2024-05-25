784e55350a04812e61b5cfa81842056e1fd0a0c7
HoAnhKhoa <traincolab2131999v1@gmail.com>
2024-05-25 13:54:25 +0700

    add code to preprocess English text

--- src/text_cls/utils/preprocess/en.py
+++ src/text_cls/utils/preprocess/en.py
@@ -1,4 +1,5 @@
 import re
+from typing import List, Text
 import pandas as pd
 import nltk
 from nltk.corpus import stopwords
@@ -7,6 +8,7 @@ from nltk.stem import PorterStemmer
 nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
+from textblob import TextBlob
 
 class EnglishTextPreprocessor:
     """
@@ -64,6 +66,41 @@ class EnglishTextPreprocessor:
         """
         return re.sub(r'[^\w\s]', '', text)
 
+    def split_sentences(
+        self,
+        text: Text
+    )-> List[Text]:
+        """
+        Splits a text string into a list of individual sentences.
+
+        This function uses a regular expression to identify sentence boundaries based
+        on punctuation and common abbreviations. It handles edge cases like
+        sentences ending in abbreviations or questions marks.
+
+        Args:
+            text (str): The text string to split into sentences.
+
+        Returns:
+            list: A list of strings, where each string is a single sentence.
+
+        Examples:
+            >>> split_sentences("This is a sentence. Is this another? It is!")
+            ['This is a sentence.', 'Is this another?', 'It is!']
+
+            >>> split_sentences("I love N.Y. It's a great city.")
+            ['I love N.Y.', "It's a great city."]
+
+            >>> split_sentences("Dr. Smith said, 'Hello there!'")
+            ['Dr', "Smith said, 'Hello there!'"]
+        """
+        text = text = re.sub("\n"," ",text)
+        text = re.sub(r"[!:\.\?\-;]\s",r"__",text)
+        text = re.sub("[\t\n\r\x0b\x0c]", '__', text)
+        # text = re.sub(r"\"\"", '__', text)
+        text = re.sub(r'\s\s+', '__', text)
+        sents = text.split('__')
+        return sents
+
     def clean_text(self, text: str) -> str:
         """
         Cleans a text string by removing special characters, converting to lowercase, and handling whitespace.
@@ -74,13 +111,16 @@ class EnglishTextPreprocessor:
         Returns:
             The cleaned text string.
         """
-        text = re.sub(r'[^a-zA-Z\s]', '', text)  # Remove special characters
+        text = re.sub(r'[^a-zA-Z_\s]', '', text)  # Remove special characters
         text = text.lower()  # Convert to lowercase
         text = text.strip()  # Remove leading/trailing whitespace
         text = re.sub(r'\s+', ' ', text)  # Replace multiple spaces with single space
         return text
 
-    def tokenize_text(self, text: str) -> list[str]:
+    def tokenize_text(
+        self,
+        text: str,
+    ) -> list[str]:
         """
         Tokenizes a text string into a list of words.
 
@@ -153,8 +193,55 @@ class EnglishTextPreprocessor:
         joined_string = separator.join(words)
         return joined_string
 
+    def spell_checking(
+        self,
+        text: str
+    ):
+        """
+        """
+        # Spell-checking using TextBlob
+        corrected_sentence = TextBlob(text).correct()
+    
+        return corrected_sentence.string
+    
+    def add_noun_phrase(
+        self,
+        text
+    )-> str:
+        """
+        Processes the input sentence by spell-checking and tokenizing it with noun phrases linked together by '_'.
+        
+        Args:
+        input_sentence (str): The sentence to be processed.
+        
+        Returns:
+        str: The processed sentence with noun phrases linked by '_'.    
+        """
 
-    def preprocess_text(self, text: str) -> list[str]:
+        # Extracting noun phrases using TextBlob
+        blob = TextBlob(text)
+        noun_phrases = blob.noun_phrases
+
+        # Tokenizing the sentence by replacing spaces in noun phrases with underscores
+        processed_sentence = blob.string
+
+        # Creating a regular expression pattern for matching noun phrases
+        for phrase in noun_phrases:
+            # Replace spaces with underscores within each noun phrase
+            underscored_phrase = '_'.join(phrase.split())
+            # Use regular expression to replace the original noun phrase in the sentence with the underscored version
+            processed_sentence = re.sub(r'\b' + re.escape(phrase) + r'\b', underscored_phrase, processed_sentence)
+
+        return processed_sentence
+    
+    def preprocess_text(
+        self,
+        text: str,
+        noun_phrase : bool = False,
+        clean_text: bool = True,
+        remove_stopwords: bool = True,
+        lemmatize_words: bool = True
+    ) -> list[str]:
         """
         Applies a full text preprocessing pipeline to a text string.
 
@@ -165,17 +252,52 @@ class EnglishTextPreprocessor:
             A list of preprocessed words.
         """
         try:
-            text = self.clean_text(text)
-            words = self.tokenize_text(text)
-            words = self.remove_stopwords(words)
-            words = self.lemmatize_words(words)
-            result = self.join_words(words)
+            sents = self.split_sentences(text)
+
+            res = []
+
+            for s in sents:
+                print(f'S BEFORE: {s}')
+
+                _s = self.spell_checking(s)
+
+                if noun_phrase:
+                    _s = self.add_noun_phrase(_s)
+
+                if clean_text:
+                    _s = self.clean_text(_s)
+            
+
+                words = self.tokenize_text(_s)
+
+                if remove_stopwords:
+                    words = self.remove_stopwords(words)
+
+                if lemmatize_words:
+                    words = self.lemmatize_words(words)
+
+                tmp_s = self.join_words(words)
+                print(f'S AFTER: {tmp_s}')
+
+                if tmp_s.strip():
+                    res.append(tmp_s)
+
         except Exception as e:
             print(f"Error: {e}")
             print(f'Text: {text}')
 
-        return result
-    def preprocess_dataframe(self, df: pd.DataFrame, text_column: str) -> pd.DataFrame:
+        print(res)
+        return ". ".join(res)
+    
+    def preprocess_dataframe(
+        self,
+        df: pd.DataFrame,
+        text_column: str,
+        noun_phrase: bool = False,
+        clean_text: bool = True,
+        remove_stopwords: bool = True,
+        lemmatize_words: bool = True       
+    ) -> pd.DataFrame:
         """
         Applies the full text preprocessing pipeline to a specified text column in a DataFrame.
 
@@ -193,7 +315,15 @@ class EnglishTextPreprocessor:
         df_processed = self.remove_nan_rows(df_processed)
 
         # Preprocess
-        df_processed[text_column] = df_processed[text_column].apply(self.preprocess_text)
+        df_processed[text_column] = df_processed[text_column].apply(
+            self.preprocess_text,
+            args= (
+                noun_phrase,
+                clean_text,
+                remove_stopwords,
+                lemmatize_words
+            )
+        )
         return df_processed
     
     def remove_nan_rows(
@@ -211,9 +341,31 @@ class EnglishTextPreprocessor:
         """
         return df.dropna(subset=['text'])
 
-if __name__ == "__main__":  
+if __name__ == "__main__":
+    # region INIT
     preprocessor = EnglishTextPreprocessor()
-    train_df = pd.read_csv("src/text_cls/dataset/20newsgroups/train.csv")
-    print(train_df.head(10))
-    preprocessed_df = preprocessor.preprocess_dataframe(train_df, 'text')
-    print(preprocessed_df.head(10))
+    train_df = pd.read_csv("src/text_cls/datasets/20newsgroups/train.csv")
+    print(train_df.head(3))
+    
+    # endregion
+
+    # region PREPROCESS
+    preprocessed_df = preprocessor.preprocess_dataframe(
+        train_df[:3],
+        'text',
+        noun_phrase = True,
+        clean_text= True,
+        remove_stopwords= True,
+        lemmatize_words = True
+    )
+
+    # endregion
+
+    # region TO CSV
+    print(preprocessed_df)
+    preprocessed_df.to_csv(
+        path_or_buf= "preprocessed_df_en.csv",
+        index = False
+    )
+
+    # endregion
