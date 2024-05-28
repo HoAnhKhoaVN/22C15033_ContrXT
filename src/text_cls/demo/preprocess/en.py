e879dfe442dd7b4e88921f479eaab96b5703ff4f
HoAnhKhoa <traincolab2131999v1@gmail.com>
2024-05-28 18:26:35 +0700

    update ref for stop word english

--- src/text_cls/demo/preprocess/en.py
+++ src/text_cls/demo/preprocess/en.py
@@ -78,12 +78,14 @@ class PreprocessEnglish(object):
         # endregion
         
         # region Load stop word on Mathematica
+        # ref: https://gist.github.com/sebleier/554280?permalink_comment_id=3525942#gistcomment-3525942
         s = """0,1,2,3,4,5,6,7,8,9,a,A,about,above,across,after,again,against,all,almost,alone,along,already,also,although,always,am,among,an,and,another,any,anyone,anything,anywhere,are,aren't,around,as,at,b,B,back,be,became,because,become,becomes,been,before,behind,being,below,between,both,but,by,c,C,can,cannot,can't,could,couldn't,d,D,did,didn't,do,does,doesn't,doing,done,don't,down,during,e,E,each,either,enough,even,ever,every,everyone,everything,everywhere,f,F,few,find,first,for,four,from,full,further,g,G,get,give,go,h,H,had,hadn't,has,hasn't,have,haven't,having,he,he'd,he'll,her,here,here's,hers,herself,he's,him,himself,his,how,however,how's,i,I,i'd,if,i'll,i'm,in,interest,into,is,isn't,it,it's,its,itself,i've,j,J,k,K,keep,l,L,last,least,less,let's,m,M,made,many,may,me,might,more,most,mostly,much,must,mustn't,my,myself,n,N,never,next,no,nobody,noone,nor,not,nothing,now,nowhere,o,O,of,off,often,on,once,one,only,or,other,others,ought,our,ours,ourselves,out,over,own,p,P,part,per,perhaps,put,q,Q,r,R,rather,s,S,same,see,seem,seemed,seeming,seems,several,shan't,she,she'd,she'll,she's,should,shouldn't,show,side,since,so,some,someone,something,somewhere,still,such,t,T,take,than,that,that's,the,their,theirs,them,themselves,then,there,therefore,there's,these,they,they'd,they'll,they're,they've,this,those,though,three,through,thus,to,together,too,toward,two,u,U,under,until,up,upon,us,v,V,very,w,W,was,wasn't,we,we'd,we'll,well,we're,were,weren't,we've,what,what's,when,when's,where,where's,whether,which,while,who,whole,whom,who's,whose,why,why's,will,with,within,without,won't,would,wouldn't,x,X,y,Y,yet,you,you'd,you'll,your,you're,yours,yourself,yourselves,you've,z,Z"""
         stop_word_mathematica = set(s.split(','))
 
         # endregion
 
         # region Load full stop word
+        # ref: https://github.com/stopwords-iso/stopwords-en/blob/master/stopwords-en.txt
         stop_word_full = set()
         with open(EN_STOP_WORD, 'r', encoding= 'utf-8') as f:
             for line in f:
