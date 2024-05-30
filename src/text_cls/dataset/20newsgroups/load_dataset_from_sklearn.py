3ea0a1cbbc3dc2bfbc5bd677027147a9259f3bdf
HoAnhKhoa <traincolab2131999v1@gmail.com>
2024-05-30 17:14:08 +0700

    demo load new20group from sklearn

---
+++ src/text_cls/dataset/20newsgroups/load_dataset_from_sklearn.py
@@ -0,0 +1,20 @@
+from sklearn.datasets import fetch_20newsgroups
+
+
+if __name__ == "__main__":
+    # Fetch the training data
+    newsgroups_train = fetch_20newsgroups(
+        subset='train',
+    )
+
+    # Fetch the test data
+    newsgroups_test = fetch_20newsgroups(subset='test')
+
+    # Print some information about the dataset
+    print("Number of training documents:", len(newsgroups_train.data))
+    print("Number of test documents:", len(newsgroups_test.data))
+    print("Target names (categories):", newsgroups_train.target_names)
+
+    # Print a sample document
+    print("\nSample document:\n", newsgroups_train.data[0])
+    print("\nCategory of the sample document:", newsgroups_train.target_names[newsgroups_train.target[0]])
