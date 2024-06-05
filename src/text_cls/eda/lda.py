2658d45d7750f1334ae74fd240a61661462acbc4
HoAnhKhoa <traincolab2131999v1@gmail.com>
2024-06-06 00:08:25 +0700

    add cli for LDA

--- src/text_cls/eda/lda.py
+++ src/text_cls/eda/lda.py
@@ -1,17 +1,15 @@
-
-# python src/text_cls/eda/lda.py
+# python src/text_cls/eda/lda.py -p src/text_cls/dataset/20newsgroups/noun_phrase/train__split_noun_phrase__remove_stopword.csv -l lda_train_en.html -c corpus.txt
+import argparse
 import pandas as pd
 import gensim
 import pyLDAvis
 import pyLDAvis.gensim
-from src.text_cls.constant import TEXT
+from src.text_cls.constant import LABEL, TEXT
 from nltk.stem import WordNetLemmatizer
-from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
 
 def load_corpus(df):
     corpus=[]
-    stem=PorterStemmer()
     lem=WordNetLemmatizer()
     for news in df[TEXT]:
         words=word_tokenize(news)
@@ -21,20 +19,36 @@ def load_corpus(df):
         corpus.append(words)
     return corpus
 
+def cli():
+    parser = argparse.ArgumentParser(description="CLI:")
+
+    # Thêm tham số vị trí (positional argument)
+    parser.add_argument("-p", '--path' ,help="Path CSV")
+
+    parser.add_argument("-l", '--html_path' ,help="Path .html to save")
+
+    parser.add_argument("-c", '--corpus_path' ,help="Path corpus to save")
+    # Phân tích các tham số đã cung cấp
+    args = parser.parse_args()
+
+    return args
 
 if __name__ == "__main__":
-    CSV_PATH = "df_remove_outlier_word.csv"
+    args = cli()
 
-    df = pd.read_csv(CSV_PATH)
+    df = pd.read_csv(args.path)
     df.dropna(axis = 0 ,how = 'any', inplace = True, ignore_index= False)
+
     corpus=load_corpus(df)
-    with open('corpus.txt', 'w', encoding='utf-8') as f:
+
+    with open(args.corpus_path, 'w', encoding='utf-8') as f:
         for word in corpus:
             f.write(f'{word}\n')
+
     dic=gensim.corpora.Dictionary(corpus)
     bow_corpus = [dic.doc2bow(doc) for doc in corpus]
 
-    n_label = len(df['label'].unique())
+    n_label = len(df[LABEL].unique())
     lda_model = gensim.models.LdaMulticore(bow_corpus,
                                    num_topics = n_label,
                                    id2word = dic,
@@ -42,5 +56,5 @@ if __name__ == "__main__":
                                    workers = 2)
     print(lda_model.show_topics())
     vis = pyLDAvis.gensim.prepare(lda_model, bow_corpus, dic)
-    pyLDAvis.save_html(vis, 'lda_remove_outlier_word.html')
+    pyLDAvis.save_html(vis, args.html_path)
 
