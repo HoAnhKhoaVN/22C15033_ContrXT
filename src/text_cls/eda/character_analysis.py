a5e43ffe55693c9bd54f13ad1c95d437de98df01
HoAnhKhoa <traincolab2131999v1@gmail.com>
2024-06-05 23:17:12 +0700

    add cli for character analysis

--- src/text_cls/eda/character_analysis.py
+++ src/text_cls/eda/character_analysis.py
@@ -1,15 +1,14 @@
-# python src/text_cls/eda/character_analysis.py
+# python src/text_cls/eda/character_analysis.py -p preprocess.csv -i box_plot_char_org.png
+import argparse
 from typing import Text
 from matplotlib import pyplot as plt
-import numpy as np
 import pandas as pd
 import seaborn as sns
-
 from src.text_cls.constant import TEXT
 
-
-def before_remove_char_outlier(
-    df: pd.DataFrame
+def draw_box_plot(
+    df: pd.DataFrame,
+    img_path : Text,
 )->None:
     """_summary_
 
@@ -23,57 +22,26 @@ def before_remove_char_outlier(
     print(f'Max: {data.max()}')
     print(f'Std: {data.std()}')
     sns.boxplot(data)
-    plt.title(f'Original Box Plot with character length')
-    plt.savefig('boxplot_char.png')
+    plt.title(f'Box Plot với độ dài của từng mẫu dữ liệu trước khi lọc lại')
+    plt.savefig(img_path)
 
-def removal_char_outlier(
-    df: pd.DataFrame,
-    ):
-    df.reset_index(inplace= True, drop=True,)
-    # IQR
-    data = df[TEXT].str.len()
-    Q1 = np.percentile(data, 25, method='midpoint')
-    Q3 = np.percentile(data, 75, method='midpoint')
-    IQR = Q3 - Q1
-    print(f'IQR: {IQR}')
+def cli():
+    parser = argparse.ArgumentParser(description="CLI: ")
 
-    lower = Q1 - 1.5*IQR
-    print(f'Lower: {lower}')
+    # Thêm tham số vị trí (positional argument)
+    parser.add_argument("-p", '--path' ,help="Path CSV")
 
-    upper_value = Q3 + 1.5*IQR
-    min_value = data.min()
-    upper = max(upper_value, min_value)
-    print(f'Upper: {upper}')
+    parser.add_argument("-i", '--img_path' ,help="Path image to save")
+    # Phân tích các tham số đã cung cấp
+    args = parser.parse_args()
 
-    # Create arrays of Boolean values indicating the outlier rows
-    upper_array = np.where(data >= upper)[0]
-    print(f'Length upper_array: {upper_array}')
-
-    lower_array = np.where(data <= lower)[0]
-    print(f'Length lower_array: {lower_array}')
-    
-    # Removing the outliers
-    df.drop(index=upper_array, inplace= True)
-    df.drop(index=lower_array, inplace= True)
-    df.reset_index(inplace= True, drop=True)
-
-    final_data = df[TEXT].str.len()
-    print(len(final_data))
-    print(f'Mean: {final_data.mean()}')
-    print(f'Min: {final_data.min()}')
-    print(f'Max: {final_data.max()}')
-    print(f'Std: {final_data.std()}')
-    
-    sns.boxplot(final_data)
-    plt.title(f'Box Plot after remove outlier')
-    plt.savefig('boxplot_char__remove_outlier.png')
-    return df
+    return args
 
 if __name__ == "__main__":
+    args = cli()
     CSV_PATH = "preprocess.csv"
 
-    df = pd.read_csv(CSV_PATH)
+    df = pd.read_csv(args.path)
     df.dropna(axis = 0 ,how = 'any', inplace = True, ignore_index= False)
+    draw_box_plot(df, args.img_path)    
 
-    df = removal_char_outlier(df)
-    df.to_csv('df_remove_outlier_char.csv', index = False)
