111146e84400e95eee6be51555de7c2123b8e593
HoAnhKhoa <traincolab2131999v1@gmail.com>
2024-07-08 21:13:50 +0700

    add code load dataset from sklearn

--- src/text_cls/dataset/20newsgroups/load_dataset_from_sklearn.py
+++ src/text_cls/dataset/20newsgroups/load_dataset_from_sklearn.py
@@ -1,4 +1,22 @@
+from typing import Any, Text
 from sklearn.datasets import fetch_20newsgroups
+from pickle import dump, load
+
+def dump_pickle(
+    fn: Text,
+    obj: Any
+)->None:
+    with open(fn, 'wb') as f:
+        dump(
+            obj= obj,
+            file = f
+        )
+
+def load_pickle(fn: Text)-> Any:
+    with open(fn, 'rb') as f:
+        data = load(f)
+    
+    return data
 
 
 if __name__ == "__main__":
@@ -6,9 +24,17 @@ if __name__ == "__main__":
     newsgroups_train = fetch_20newsgroups(
         subset='train',
         remove=('header','footer','quotes'),
-        categories=['rec.autos'],
         shuffle= False
     )
+
+    plk_path = 'newsgroups_train.plk'
+    dump_pickle(
+        fn = plk_path,
+        obj= newsgroups_train
+    )
+
+    newsgroups_train_ = load_pickle(plk_path)
+
 
     # # Fetch the test data
     # newsgroups_test = fetch_20newsgroups(subset='test')
@@ -19,5 +45,5 @@ if __name__ == "__main__":
     # print("Target names (categories):", newsgroups_train.target_names)
 
     # Print a sample document
-    print("\nSample document:\n", newsgroups_train.data[0])
-    print("\nCategory of the sample document:", newsgroups_train.target_names[newsgroups_train.target[0]])
+    print("\nSample document:\n", newsgroups_train_.data[0])
+    print("\nCategory of the sample document:", newsgroups_train_.target_names[newsgroups_train.target_[0]])
